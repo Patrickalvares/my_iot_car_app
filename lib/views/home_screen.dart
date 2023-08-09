3baba1e68e6aa52bf1d635bef5ff27369856294f
Notebook Patrick <patrickalvares@outlook.com>
2023-08-09 09:59:35 -0300

    Joystick updates Firebase realtime database

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_iot_car_app/components/joystick.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,14 +10,21 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final databaseReference = FirebaseDatabase.instance.reference();
+
   double xValue = 0;
   double yValue = 0;
 
   void _onJoystickChanged(double x, double y) {
     setState(() {
       xValue = x;
-      yValue = y;
+      yValue = y * -1;
     });
+
+    // Atualizar os valores no Firebase Realtime Database
+    databaseReference.child("forward").set(y <= 0);
+    databaseReference.child("speed").set(y.abs());
+    databaseReference.child("steeringAngle").set(x);
   }
 
   @override
